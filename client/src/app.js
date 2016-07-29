2996e3c4460953b40bf91cf0656032dfedfd442c
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-07-29 11:10:25 +0100

    initial commit

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+var TestModel = require('./models/test.js')
+var TestModelView = require('./views/test.js')
+
+
+
+window.onload = function(){
+  var test = new TestModel('Jazz');
+  var testView = new TestModelView(test);
+  testView.changeHeader();
+}
